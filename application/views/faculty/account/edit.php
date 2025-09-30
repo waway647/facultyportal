7fb6303a06d6517b371f97c9cbe6164ca7c70f79
seanlabrador0625 <2022-02459@sanbeda.edu.ph>
2025-09-30 14:22:58 +0800

    Update edit.php

--- application/views/faculty/account/edit.php
+++ application/views/faculty/account/edit.php
@@ -19,131 +19,131 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <body>
   <div class="dashboard-faculty">
       <div class="nav-container">
-        <div class="portal-logo">
-          <div class="role-item">
-            <div class="img-container"><img class="img" src="<?php echo base_url('assets/images/logo/sbu_logo.svg'); ?>" /></div>
-            <div class="role-heading">
-              <div class="text-wrapper">San Beda University</div>
-              <div class="div">Faculty Portal</div>
-            </div>
-          </div>
-        </div>
-		<!-- Modal -->
-		<div id="postAnnouncementModal" class="modal">
-			<div class="modal-content">
-				<div class="postAnnouncement-container">
-					<div class="postmodal-heading">
-						<div class="div">Post New Announcement</div>
-					</div>
-					<div class="postmodal-form-container">
-						<!-- Title Input -->
-						<div class="postmodal-form-input">
-							<input type="text" id="announement_title" name="announement_title" placeholder="Title">
-						</div>
-						<!-- Custom Textarea for Announcement Body -->
-						<div class="postmodal-form-input">
-							<div id="announcement_body" class="custom-textarea" contenteditable="true" placeholder="Write your announcement here..."></div>
-							<div class="attachment-container">
-								<label for="announcement_attachment" class="attachment-button">
-									<img src="https://cdn-icons-png.flaticon.com/512/54/54719.png" alt="">
-									Attach Files
-								</label>
-								<input type="file" id="announcement_attachment" name="announcement_attachment" accept=".jpg,.jpeg,.png,.pdf,.doc,.docx" hidden>
-								<div id="attachment_preview" class="attachment-preview"></div>
-							</div>
-						</div>
-					</div>
-					<!-- Buttons -->
-					<div class="button-container">
-						<input type="submit" value="Post">
-						<a href="javascript:void(0);" id="closeModalBtn">
-							<div class="cancel-button">
-								<h6>Cancel</h6>
-							</div>
-						</a>
+			<div class="portal-logo">
+				<div class="role-item">
+					<div class="img-container"><img class="img" src="<?php echo base_url('assets/images/logo/sbu_logo.svg'); ?>" /></div>
+					<div class="role-heading">
+					<div class="text-wrapper">San Beda University</div>
+					<div class="div">Faculty Portal</div>
 					</div>
 				</div>
 			</div>
-		</div>
-        <div class="nav-links-container">
-			<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Dashboard/index">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/dash.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Dashboard</div></div>
+			<!-- Modal -->
+			<div id="postAnnouncementModal" class="modal">
+				<div class="modal-content">
+					<div class="postAnnouncement-container">
+						<div class="postmodal-heading">
+							<div class="div">Post New Announcement</div>
+						</div>
+						<div class="postmodal-form-container">
+							<!-- Title Input -->
+							<div class="postmodal-form-input">
+								<input type="text" id="announement_title" name="announement_title" placeholder="Title">
+							</div>
+							<!-- Custom Textarea for Announcement Body -->
+							<div class="postmodal-form-input">
+								<div id="announcement_body" class="custom-textarea" contenteditable="true" placeholder="Write your announcement here..."></div>
+								<div class="attachment-container">
+									<label for="announcement_attachment" class="attachment-button">
+										<img src="https://cdn-icons-png.flaticon.com/512/54/54719.png" alt="">
+										Attach Files
+									</label>
+									<input type="file" id="announcement_attachment" name="announcement_attachment" accept=".jpg,.jpeg,.png,.pdf,.doc,.docx" hidden>
+									<div id="attachment_preview" class="attachment-preview"></div>
+								</div>
+							</div>
+						</div>
+						<!-- Buttons -->
+						<div class="button-container">
+							<input type="submit" value="Post">
+							<a href="javascript:void(0);" id="closeModalBtn">
+								<div class="cancel-button">
+									<h6>Cancel</h6>
+								</div>
+							</a>
+						</div>
+					</div>
 				</div>
-			</a>
-          
-		  	<a href="http://localhost/GitHub/facultyportal/index.php/common_controllers/Announcements/index">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/announce.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Announcements</div></div>
-				</div>
-			</a>
-          
-		  	<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Profile/index">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/profile.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Profile</div></div>
-				</div>
-			</a>
-          
-		  	<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Courses/index">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/course.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Courses</div></div>
-				</div>
-			</a>
-          
-		  	<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/ResearchOutputs/index">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/research.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Research Outputs</div></div>
-				</div>
-			</a>
-    
-		  	<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Consultations/index">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/consult.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Consultations</div></div>
-				</div>
-			</a>
-        </div>
-        <div class="nav-links-container-2">
+			</div>
+			<div class="nav-links-container">
+				<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Dashboard/index">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/dash.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Dashboard</div></div>
+					</div>
+				</a>
+			
+				<a href="http://localhost/GitHub/facultyportal/index.php/common_controllers/Announcements/index">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/announce.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Announcements</div></div>
+					</div>
+				</a>
+			
+				<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Profile/index">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/profile.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Profile</div></div>
+					</div>
+				</a>
+			
+				<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Courses/index">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/course.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Courses</div></div>
+					</div>
+				</a>
+			
+				<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/ResearchOutputs/index">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/research.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Research Outputs</div></div>
+					</div>
+				</a>
+		
+				<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Consultations/index">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/consult.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Consultations</div></div>
+					</div>
+				</a>
+			</div>
+			<div class="nav-links-container-2">
 
-				<!-- Notification Button -->
-			<a href="javascript:void(0);" id="notificationBtn">
-				<div class="nav-link">
-					<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/notif.svg'); ?>" /></div>
-					<div class="frame-2"><div class="text-wrapper-4">Notifications</div></div>
-				</div>
-			</a>
-			
-			<!-- Slide-in Notification Panel -->
-			<div id="notificationPanel" class="notification-panel"></div>
-			
-			<!-- Your profile link and other content here -->
-			<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Account/index">
-				<div class="nav-link-3">
-					<?php if (isset($faculty) && $faculty !== null): ?>
-					<div class="img-wrapper">
-						<img class="img-account" src="<?php echo base_url(!empty($faculty->profile_picture) ? $faculty->profile_picture : 'assets/images/profile/default_profile.png'); ?>" alt="Profile Picture">
+					<!-- Notification Button -->
+				<a href="javascript:void(0);" id="notificationBtn">
+					<div class="nav-link">
+						<div class="image-wrapper"><img class="img" src="<?php echo base_url('assets/images/icon/notif.svg'); ?>" /></div>
+						<div class="frame-2"><div class="text-wrapper-4">Notifications</div></div>
 					</div>
-					
-					
-					<div class="frame-3">
-						<div class="text-wrapper-5" id="full_name"></div>
-						<div class="text-wrapper-6"></div>
+				</a>
+				
+				<!-- Slide-in Notification Panel -->
+				<div id="notificationPanel" class="notification-panel"></div>
+				
+				<!-- Your profile link and other content here -->
+				<a href="http://localhost/GitHub/facultyportal/index.php/faculty_controllers/Account/index">
+					<div class="nav-link-3">
+						<?php if (isset($faculty) && $faculty !== null): ?>
+						<div class="img-wrapper">
+							<img class="img-account" src="<?php echo base_url(!empty($faculty->profile_picture) ? $faculty->profile_picture : 'assets/images/profile/default_profile.png'); ?>" alt="Profile Picture">
+						</div>
+						
+						
+						<div class="frame-3">
+							<div class="text-wrapper-5" id="full_name"></div>
+							<div class="text-wrapper-6"></div>
+						</div>
+						<?php endif ?>
 					</div>
-					<?php endif ?>
-				</div>
-			</a>
-          
-        </div>
-        <div class="nav-link-wrapper">
-          <div class="frame-wrapper">
-            <div class="frame-4"><div class="text-wrapper-7">v1.0.1</div></div>
-          </div>
-        </div>
+				</a>
+			
+			</div>
+			<div class="nav-link-wrapper">
+			<div class="frame-wrapper">
+				<div class="frame-4"><div class="text-wrapper-7">v1.0.1</div></div>
+			</div>
+			</div>
       </div>
       <div class="main-content">
         <div class="main-content-2">
